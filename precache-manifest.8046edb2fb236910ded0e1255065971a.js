3c61cdf715f44d6b4aa994286617d24b4ed01181
imagh <akashghosh@outlook.in>
2020-12-01 01:45:23 +0530

    Updates

---
+++ precache-manifest.8046edb2fb236910ded0e1255065971a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d905df3ad74fac24a7e3cfa2ef598b6e",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "68a5b69a5c2d39a46bbd",
+    "url": "/profile/static/css/main.ab12954b.chunk.css"
+  },
+  {
+    "revision": "1d88e957f0441513b8cb",
+    "url": "/profile/static/js/2.1787e0d0.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/profile/static/js/2.1787e0d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a5b69a5c2d39a46bbd",
+    "url": "/profile/static/js/main.568192b3.chunk.js"
+  },
+  {
+    "revision": "86849911fc80171a92c2",
+    "url": "/profile/static/js/runtime-main.dd7f91f9.js"
+  },
+  {
+    "revision": "6d3062f6f6916e611558dbfe6be5b748",
+    "url": "/profile/static/media/Resume_07NOV2020.6d3062f6.pdf"
+  },
+  {
+    "revision": "bdf26b79ebd889e3acfcdec8c401376b",
+    "url": "/profile/static/media/advanced-computer-networks.bdf26b79.jpeg"
+  },
+  {
+    "revision": "5f95d6cb14f5f8bd7971425176bc7559",
+    "url": "/profile/static/media/big-data.5f95d6cb.jpeg"
+  },
+  {
+    "revision": "45c0839d5e8a3c6e2d4fa75e193ca742",
+    "url": "/profile/static/media/computer-networks.45c0839d.png"
+  },
+  {
+    "revision": "8e9a0a1854d509d6705aad45f8f85d11",
+    "url": "/profile/static/media/future-internet.8e9a0a18.jpeg"
+  },
+  {
+    "revision": "2544b89c54e3fd3aa795cea71de5c79d",
+    "url": "/profile/static/media/information-security.2544b89c.jpeg"
+  },
+  {
+    "revision": "cbfc9e6c72cfc7bed76e116a9d59ed95",
+    "url": "/profile/static/media/logo-download.cbfc9e6c.svg"
+  },
+  {
+    "revision": "6567d01e948ab8d96c24352c4e65ca9b",
+    "url": "/profile/static/media/logo-ellipsis.6567d01e.svg"
+  },
+  {
+    "revision": "0f7b9ab773a936b6942c1a44214b4abf",
+    "url": "/profile/static/media/logo-facebook.0f7b9ab7.svg"
+  },
+  {
+    "revision": "a3a97e11bef0768004d32aace5f09b97",
+    "url": "/profile/static/media/logo-github.a3a97e11.svg"
+  },
+  {
+    "revision": "84e36408f7623fe0f450822113dbd03c",
+    "url": "/profile/static/media/logo-linkedin.84e36408.svg"
+  },
+  {
+    "revision": "90528849a98fd7651a25fb3ac97adcf4",
+    "url": "/profile/static/media/logo-menu.90528849.svg"
+  },
+  {
+    "revision": "1eb4fd43e5496b37d284adcb55065288",
+    "url": "/profile/static/media/logo-twitter.1eb4fd43.svg"
+  },
+  {
+    "revision": "543657ee6defe49dcae5f43ededa945f",
+    "url": "/profile/static/media/mail.543657ee.svg"
+  },
+  {
+    "revision": "c117fc02facd7abb5d00380e49388d63",
+    "url": "/profile/static/media/male-profile-picture.c117fc02.png"
+  },
+  {
+    "revision": "dfb1300ef8c8ede8aaded7275a4de88a",
+    "url": "/profile/static/media/network-security.dfb1300e.jpeg"
+  },
+  {
+    "revision": "c75205fb9c16651ba478707fb197137e",
+    "url": "/profile/static/media/scholar.c75205fb.svg"
+  }
+]);
