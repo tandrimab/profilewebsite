094a6e863c05b9378d3219080224770574c7a27d
imagh <akashghosh@outlook.in>
2020-12-03 00:03:27 +0530

    Bug fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,16 +16,27 @@ import '../css/styles.css'
 function App(props) {
   function initialDynamicSetup() {
     if (document.location.hash === "") {
-      var scrollDistance = document.getElementById('scroll-snap-container').scrollTop;
+      var scrollDistance = Math.ceil(document.getElementById('scroll-snap-container').scrollTop);
       if (scrollDistance === 0) {
         document.querySelector("#navmenu li a").classList.add('active');
       } else {
         var links = document.getElementById("navmenu").getElementsByTagName("a");
-        Array.from(links).forEach((link) => {
-          if (document.getElementById(link.id.replace('a-', '')).offsetTop <= scrollDistance) {
-            link.classList.add('active');
+
+        if (links.length > 0) {
+          var prev = links[0];
+          if (document.getElementById(prev.id.replace('a-', '')).offsetTop <= scrollDistance) {
+            prev.classList.add('active');
           }
-        });
+          for (let i = 1; i < links.length; i++) {
+            const link = links[i];
+            if (document.getElementById(link.id.replace('a-', '')).offsetTop > scrollDistance) {
+              break;
+            }
+            prev.classList.remove('active');
+            link.classList.add('active');
+            prev = link;
+          }
+        }
       }
     } else {
       if (document.getElementById(document.location.hash.replace('#', '')) != null) {
@@ -36,7 +47,7 @@ function App(props) {
   }
 
 	function onScrollListener() {
-    var scrollDistance = document.getElementById('scroll-snap-container').scrollTop;
+    var scrollDistance = Math.ceil(document.getElementById('scroll-snap-container').scrollTop);
     
     var links = document.getElementById("navmenu").getElementsByTagName("a");
     Array.from(links).forEach((link) => {
