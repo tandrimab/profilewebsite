13a0dcd4620d83458a579cafe3300ed247b0a4c8
Tandrima Bhattacharjee <bhattacharjee.tan@outlook.com>
2020-12-21 14:37:52 +0530

    teaching images, footer linkedin profile added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Footer() {
     return (
         <div id="footer">
-            <p>Powered by <span> <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React.js</a></span></p>
+            <p>Powered by <span> <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React.js</a></span>&nbsp;&middot;&nbsp;<span>Developed by &nbsp;
+            <a href="https://www.linkedin.com/in/tandrima-bhattacharjee-508508144/">Tandrima</a>&nbsp;&&nbsp; 
+            <a href="https://www.linkedin.com/in/akash-ghosh-695958111/">Akash</a></span></p>
         </div>
     )
 }
