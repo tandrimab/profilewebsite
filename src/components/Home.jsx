6311e858da8c79f955d9b2eba5f7794dc0a3fb97
debopambhattacherjee <debopam.bhattacherjee@inf.ethz.ch>
2021-02-23 12:16:37 +0100

    home alert added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,6 +21,7 @@ function Home(props) {
             <p className="desgn-dept-group">{profile.designation} @ <a href={profile.department.link} target='_blank' rel="noopener noreferrer">{profile.department.name}</a></p>
             <p className="desgn-dept-group"><a href='https://www.systems.ethz.ch/' target='_blank' rel="noopener noreferrer">Systems Group</a> | <a href='https://www.ethz.ch/en.html' target='_blank' rel="noopener noreferrer">ETH Zürich</a></p>
             <SocialBar social={profile.social} />
+            <p className="homealert">Available on the job market</p>
           </div>
         </div>
         <div id="about-me">
