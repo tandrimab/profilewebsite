bee4f290c0d976849586f6019d9f1dd1c1a1a308
Debopam Bhattacherjee <debopam@Debopams-MacBook-Pro-2.local>
2021-08-09 16:41:20 +0200

    nsdi22 added

--- src/data/papers.js
+++ src/data/papers.js
@@ -2,10 +2,7 @@ export const papers = [
   {
     "title": "cISP: A Speed-of-Light Internet Service Provider",
     "authors": "Debopam Bhattacherjee *, Waqar Aqeel *, Sangeetha Abdu Jyothi, Ilker Nadi Bozkurt, William Sentosa, Muhammad Tirmazi, Anthony Aguirre, Balakrishnan Chandrasekaran, P. Brighten Godfrey, Gregory P. Laughlin, Bruce M. Maggs, Ankit Singla",
-    "event": "USENIX NSDI 2022 [To appear]",
-    "links": [
-      { 'text': 'arXiv 2018 version', 'link': 'arxiv18_cisp.pdf' }
-    ]
+    "event": "USENIX NSDI 2022 [To appear]"
   },{
     "title": "In-orbit computing: an outlandish thought experiment?",
     "authors": "Debopam Bhattacherjee *, Simon Kassing *, Melissa Licciardello, Ankit Singla",
