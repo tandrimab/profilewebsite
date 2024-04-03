c4f026b88e16d8c4215fcc885d9a5f7a3ea0b0b8
akashg-icm <akash.ghosh@incredmoney.com>
2024-04-03 23:16:36 +0530

    Updated the deprecated react-particles package responsible for the network animation

--- src/components/ParticlesContainer.jsx
+++ src/components/ParticlesContainer.jsx
@@ -1,32 +1,55 @@
-import React from "react";
-import Particles from "react-particles-js";
+import React, { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
 
 function ParticlesContainer() {
     const maxViewPortLength = Math.max(window.innerHeight, window.innerWidth);
     const numOfParticles = maxViewPortLength / 16;
+
+    const particlesInit = useCallback(async engine => {
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
+
     return (
         <div id="particles-div">
-            <Particles 
-                params={{
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
                     particles: {
-						color: {
-							value: "#ADB5BD"
-						},
-						line_linked: {
-							color: {
-								value: "#ADB5BD"
-							}
-						},
-						number: {
-							value: numOfParticles
-						},
-						size: {
-							value: 2
-						},
+                        color: {
+                            value: "#ADB5BD"
+                        },
+                        links: {
+                            color: "#ADB5BD",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
                         move: {
-                            speed: 1
-                        }
-					},
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce",
+                            },
+                            random: false,
+                            speed: 2,
+                            straight: false,
+                        },
+                        number: {
+                            value: numOfParticles
+                        },
+                        size: {
+                            value: 2
+                        },
+                    },
                     interactivity: {
                         events: {
                             onhover: {
